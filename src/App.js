38fd43dbe3c650a6dce8cd1336cbd5fca134fd24
GavireddyMadhav <gavireddymadhavreddy@gmail.com>
2022-01-22 12:55:55 +0530

    Third

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Results from './component/Results';
+import AgeCal from './component/AgeCal';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Results/> */}
+      <AgeCal/>
+    </div>
+  );
+}
+
+export default App;
